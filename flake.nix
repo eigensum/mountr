5c56bdce5b499e50a1c5fe958e11c047d989d3c7
A. Thallemer <airan.thallemer@tum.de>
2026-03-29 23:15:19 +0200

    initialized rust project

--- flake.nix
+++ flake.nix
@@ -1,23 +1,9 @@
 {
-  description = "Rust development environment for Mountr";
+  description = "Development Shell for mountr";
 
   inputs = {
-    pkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   };
 
-  outputs = {
-    self,
-    pkgs,
-  }: let
-    system = "x86_64-linux";
-    pkgs = import pkgs {inherit system;};
-  in {
-    devShells.${system}.default = pkgs.mkShell {
-      packages = with pkgs; [rustup];
-
-      shellHook = ''
-        rustc --version
-      '';
-    };
+  outputs = inputs: {
   };
 }
