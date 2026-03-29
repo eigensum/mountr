b692b242a9cae06d0e29203ba3ef774c0bd7bf17
A. Thallemer <airan.thallemer@tum.de>
2026-03-29 23:04:50 +0200

    fixed flake

--- flake.nix
+++ flake.nix
@@ -2,14 +2,17 @@
   description = "Rust development environment for Mountr";
 
   inputs = {
-    pkgs.url = "github:NixOS/nixpkgs/nixos-25-11";
+    pkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   };
 
   outputs = {
     self,
     pkgs,
-  }: {
-    devShells.default = pkgs.mkShell {
+  }: let
+    system = "x86_64-linux";
+    pkgs = import pkgs {inherit system;};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
       packages = with pkgs; [rustup];
 
       shellHook = ''
